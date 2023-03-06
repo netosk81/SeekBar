9e89dec1aa87fdb769067b71e5aee287d0b06e6d
netosk81 <hk.neto@gmail.com>
2023-03-06 20:33:17 -0300

    Initial commit

---
+++ app/src/main/java/com/example/seekbar/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.seekbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBarEscala;
+    private TextView textResultado;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBarEscala = findViewById(R.id.seekBarEscala);
+        textResultado = findViewById(R.id.textResultado);
+
+        seekBarEscala.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textResultado.setText("Pogresso: " + i + " / " + seekBar.getMax());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //textResultado.setText("onStartTrackingTouch");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //textResultado.setText("onStopTrackingTouch");
+            }
+        });
+    }
+
+    public void recuperarProgresso(View view){
+
+        textResultado.setText("Escolhido: " + seekBarEscala.getProgress());
+
+    }
+}
